63805f94b19657b9e31cd36f34e8fa32996d729a
ky05h1n <ky05h1n@ky05h1n>
2022-08-12 12:08:51 -0400

    philo

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -3,14 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: enja <enja@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: ky05h1n <ky05h1n@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 17:03:18 by enja              #+#    #+#             */
-/*   Updated: 2022/08/11 18:58:48 by enja             ###   ########.fr       */
+/*   Updated: 2022/08/12 12:07:05 by ky05h1n          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+int	input_num(char *av)
+{
+	int	i;
+	int	some;
+
+	some = 0;
+	i = 0;
+	while (av[i])
+	{
+		some = some * 10 + (av[i] - '0');
+		i++;
+	}
+	return (some);
+}
 
 int	input_argcheck(int ac, char **av)
 {
@@ -36,13 +51,25 @@ int	input_argcheck(int ac, char **av)
 
 int	main(int ac, char **av)
 {
+	t_data	ph_data;
+	t_data	*ptr;
+	int		n;
+
+	ptr = &ph_data;
 	(void)av;
+	n = 0;
 	if (ac == 5)
 	{
 		if (input_argcheck(ac, av) == 0)
 			return (0);
 		else
-			printf("its good");
+		{
+			while (av[n])
+			{
+				ptr->num_philo = input_num(av[n]);
+				ptr->time_to_die = input_num(av[n + 1]);
+			}
+		}
 	}
 	else
 		printf("input error\n");
--- philo/philosophers.h
+++ philo/philosophers.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: enja <enja@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: ky05h1n <ky05h1n@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 17:04:36 by enja              #+#    #+#             */
-/*   Updated: 2022/08/11 17:09:02 by enja             ###   ########.fr       */
+/*   Updated: 2022/08/12 12:03:12 by ky05h1n          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,12 @@
 
 # include <stdio.h>
 # include <stdlib.h>
+
+typedef struct t_data{
+	int	num_philo;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+}t_data;
 
 #endif
